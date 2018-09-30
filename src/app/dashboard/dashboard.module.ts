66181daee3a2eec9ac69f13c2689e21c2fe448b1
Lucas Calebe <calebematos@gmail.com>
2018-09-30 12:26:26 -0300

    Adicionando graficos com valores fixos

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { SharedModule } from 'primeng/components/common/shared';
+import {ChartModule} from 'primeng/chart';
+import {PanelModule} from 'primeng/panel';
+
+
+import { SharedModule } from './../shared/shared.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
     CommonModule,
+
+    PanelModule,
+    ChartModule,
 
     SharedModule,
     DashboardRoutingModule
